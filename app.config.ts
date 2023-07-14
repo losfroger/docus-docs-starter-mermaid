7b557258269ec258b2070862a675e8ee9934a7ec
Emilio P <ea.pugaavalos.99@gmail.com>
2023-07-13 22:54:16 -0600

    Change app config to point to the repo

--- app.config.ts
+++ app.config.ts
@@ -4,8 +4,14 @@ export default defineAppConfig({
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      github: 'losfroger/docus-docs-starter-mermaid'
+    },
+    github: {
+      dir: './content',
+      branch: 'main',
+      repo: 'docus-docs-starter-mermaid',
+      owner: 'losfroger',
+      edit: true
     },
     aside: {
       level: 0,
